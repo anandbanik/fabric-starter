bf6675f18f83af393c7246378a46a9661acf1b96
Oleg Abdrashitov <olegabu73@gmail.com>
2017-11-26 14:35:57 -0500

    add move query

--- chaincode/go/payment/chaincode_payment.go
+++ chaincode/go/payment/chaincode_payment.go
@@ -6,67 +6,39 @@ import (
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
+	"strings"
+	"encoding/pem"
+	"crypto/x509"
 )
+
+var logger = shim.NewLogger("PaymentChaincode")
 
 type PaymentChaincode struct {
 }
 
 func (t *PaymentChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ex02 Init")
-	_, args := stub.GetFunctionAndParameters()
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
+	logger.Debug("Init")
 	return shim.Success(nil)
 }
 
 func (t *PaymentChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ex02 Invoke")
+	logger.Debug("Invoke")
 	function, args := stub.GetFunctionAndParameters()
 	if function == "move" {
-		// Make payment of X units from A to B
 		return t.move(stub, args)
-	} else if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
+	} else if function == "add" {
+		return t.add(stub, args)
 	} else if function == "query" {
-		// the old "Query" is now implemtned in invoke
 		return t.query(stub, args)
 	}
 
-	return shim.Error("Invalid invoke function name. Expecting \"move\" \"delete\" \"query\"")
+	return pb.Response{Status:403, Message:"unknown function name"}
 }
 
-// Transaction makes payment of X units from A to B
+func (t *PaymentChaincode) add(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
 func (t *PaymentChaincode) move(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
@@ -74,14 +46,12 @@ func (t *PaymentChaincode) move(stub shim.ChaincodeStubInterface, args []string)
 	var err error
 
 	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
+		return pb.Response{Status:403, Message:"incorrect number of arguments"}
 	}
 
 	A = args[0]
 	B = args[1]
 
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
 	Avalbytes, err := stub.GetState(A)
 	if err != nil {
 		return shim.Error("Failed to get state")
@@ -123,55 +93,40 @@ func (t *PaymentChaincode) move(stub shim.ChaincodeStubInterface, args []string)
 	return shim.Success(nil)
 }
 
-// Deletes an entity from state
-func (t *PaymentChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	A := args[0]
-
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
+func (t *PaymentChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	creatorBytes, err := stub.GetCreator()
 	if err != nil {
-		return shim.Error("Failed to delete state")
+		return shim.Error("cannot get creator")
 	}
 
-	return shim.Success(nil)
+	name, org := getCreator(creatorBytes)
+
+	key := name + "@" + org
+
+	bytes, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error("cannot get state")
+	}
+
+	return shim.Success(bytes)
 }
 
-// query callback representing the query of a chaincode
-func (t *PaymentChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A string // Entities
-	var err error
+var getCreator = func (certificate []byte) (string, string) {
+	data := certificate[strings.Index(string(certificate), "-----"): strings.LastIndex(string(certificate), "-----")+5]
+	block, _ := pem.Decode([]byte(data))
+	cert, _ := x509.ParseCertificate(block.Bytes)
+	organization := cert.Issuer.Organization[0]
+	commonName := cert.Subject.CommonName
+	logger.Debug("commonName: " + commonName + ", organization: " + organization)
 
-	if len(args) != 1 {
-		//return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
-		return shim.Success(nil)
-	}
+	organizationShort := strings.Split(organization, ".")[0]
 
-	A = args[0]
-
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
+	return commonName, organizationShort
 }
 
 func main() {
 	err := shim.Start(new(PaymentChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting chaincode: %s", err)
 	}
 }
